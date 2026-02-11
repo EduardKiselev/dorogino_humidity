87cec25ec5da2b2811fd809897f7583f6486f904
EduardKiselev <e.kiselev88@gmail.com>
2026-02-11 12:20:40 +0300

    1

---
+++ remove_server/restart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+cd /root/humidity/dorogino_humidity/remove_server
+
+git pull origin main
+docker-compose down
+docker system prune -f
+docker-compose up -d --build
+
+echo "✅ Готово"
