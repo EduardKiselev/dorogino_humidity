dc36fe807554ce464948f265ae48b9d7ee0210ba
EduardKiselev <e.kiselev88@gmail.com>
2026-02-12 16:45:27 +0300

    1

--- remove_server/front/config.py
+++ remove_server/front/config.py
@@ -11,7 +11,6 @@ class Config:
     DB_NAME = os.environ.get('DB_NAME', 'sensor_data')
     DB_USER = os.environ.get('DB_USER', 'postgres')
     DB_PASSWORD = os.environ.get('DB_PASSWORD', 'password')
-    print(DB_HOST,DB_PORT,DB_NAME,DB_USER,DB_PASSWORD)
     
     SQLALCHEMY_DATABASE_URI = (
         f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
