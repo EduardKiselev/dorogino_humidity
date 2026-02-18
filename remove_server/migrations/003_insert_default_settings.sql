e2ccaac9fb2f927d952dc0da223de3df1d18a7a6
Eduard <e.kiselev@centraluniversity.ru>
2026-02-18 12:10:05 +0300

    add migrations

--- remove_server/migrations/003_insert_default_settings.sql
+++ remove_server/migrations/003_insert_default_settings.sql
@@ -1,20 +1,8 @@
-CREATE OR REPLACE FUNCTION fill_settings() RETURNS void AS $$
-DECLARE
-    sensor_id INTEGER;
-    hour_val INTEGER;
-BEGIN
-    FOR sensor_id IN 1..5 LOOP
-        FOR hour_val IN 0..23 LOOP
-            INSERT INTO settings (sensor_id, hour_of_day, humidity, histeresys_up, histeresys_down)
-            VALUES (sensor_id, hour_val, 60.0, 5.0, 5.0)
-            ON CONFLICT (sensor_id, hour_of_day) DO UPDATE SET
-                humidity = EXCLUDED.humidity,
-                histeresys_up = EXCLUDED.histeresys_up,
-                histeresys_down = EXCLUDED.histeresys_down;
-        END LOOP;
-    END LOOP;
-END;
-$$ LANGUAGE plpgsql;
-
-SELECT fill_settings();
-DROP FUNCTION fill_settings();
+INSERT INTO settings (sensor_id, hour_of_day, humidity, histeresys_up, histeresys_down)
+SELECT s.sensor_id, h.hour_of_day, 60.0, 5.0, 5.0
+FROM generate_series(1, 5) AS s(sensor_id)
+CROSS JOIN generate_series(0, 23) AS h(hour_of_day)
+ON CONFLICT (sensor_id, hour_of_day) DO UPDATE SET
+    humidity = EXCLUDED.humidity,
+    histeresys_up = EXCLUDED.histeresys_up,
+    histeresys_down = EXCLUDED.histeresys_down;
