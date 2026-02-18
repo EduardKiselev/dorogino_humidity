8738bbe47fe0ef31fdd95ad37daa9e2d5ed91553
EduardKiselev <e.kiselev88@gmail.com>
2026-02-18 14:08:59 +0300

    1

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e  # Остановить при любой ошибке
+
+cd /home/kiseleveduard/dev/dorogino_humidity
+
+git pull
+
+docker compose down
+
+# Собираем (если нужно обновлять код)
+echo "🔨 Building services..."
+docker compose up --build -d
+
+# Очистка: удаляем неиспользуемые образы, контейнеры, сети
+echo "🧹 Cleaning unused Docker resources..."
+docker system prune -f 
+
+echo "✅ dorogino_humidity started and cleaned up."
+
