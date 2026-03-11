b018709a0fca700060b069e68c59971723c66040
Eduard <e.kiselev@centraluniversity.ru>
2026-03-11 10:11:50 +0300

    work with picts

--- remove_server/migrations/005_add_destination_ip.sql
+++ remove_server/migrations/005_add_destination_ip.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 ALTER TABLE sensor_readings RENAME COLUMN ip_address TO source_ip;
 
-ALTER TABLE sensor_readings ADD COLUMN destination_ip VARCHAR(50) DEFAULT '192.168.0.100';
+ALTER TABLE sensor_readings ADD COLUMN destination_ip VARCHAR(50) DEFAULT '192.168.10.100';
 
 COMMIT;
