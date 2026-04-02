0cc1a53affdadda9455608457dfeaf4a6c6124a2
EduardKiselev <e.kiselev88@gmail.com>
2026-04-02 21:04:16 +0300

    work with workshop shceme

---
+++ remove_server/migrations/009_change_time_to_utc.sql
@@ -0,0 +1,9 @@
+-- Предварительно сделайте бэкап!
+-- Конвертируем naive-время (которое было в GMT+7) в UTC:
+UPDATE sensor_readings 
+SET timestamp = timestamp AT TIME ZONE 'Asia/Novosibirsk' AT TIME ZONE 'UTC'
+WHERE timestamp IS NOT NULL;
+
+-- Затем измените тип колонки в БД на TIMESTAMPTZ:
+ALTER TABLE sensor_readings 
+ALTER COLUMN timestamp TYPE TIMESTAMPTZ USING timestamp AT TIME ZONE 'UTC';
