3233a5e64cc8323c76e18be84cabab6972312dbf
EduardKiselev <e.kiselev88@gmail.com>
2026-04-02 11:13:25 +0300

    work with workshop shceme

--- remove_server/front/app.py
+++ remove_server/front/app.py
@@ -1,7 +1,7 @@
 # app.py
 from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory, abort, jsonify
 from config import Config
-from models import db, SensorReading, Setting, SettingChangeLog, ControllerStatus, ScreenRecord
+from models import db, SensorReading, Setting, SettingChangeLog, ControllerStatus, ScreenRecord, SensorLocation
 from datetime import datetime, timezone, timedelta
 import pandas as pd
 from zoneinfo import ZoneInfo
